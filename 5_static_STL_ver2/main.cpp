525ec4612bae9aa9f192343018dddf3456bed09a
Luis Fernando Santuario Parra <Santuarioparral@hotmail.com>
2019-08-27 12:53:16 -0500

    Programas completos
    Añadidos ambos programas

--- 5_static_STL_ver2/main.cpp
+++ 5_static_STL_ver2/main.cpp
@@ -6,24 +6,56 @@
 using namespace std;
 
 int main() {
+/*
+    int n =100;
+
     srand(time(NULL));
-    PoligonoIrreg a, b;
+    PoligonoIrreg a;
 
-    vector<PoligonoIrreg> AA;
-    AA.reserve(1000);
+    vector<PoligonoIrreg> poligonos;
+    poligonos.reserve(10);
 
-    for (int i = 0; i < rand() % 100; ++i) {
-        Coordenada xy(rand() % 100, rand() % 100);
-        a.addVertice(xy);
+    for (int x = 0; x<n ; x++){
+        if(poligonos.size()<n){
+            poligonos.resize(poligonos.size()+1);
+            for (int i = 0; i < rand() % 100; ++i) {
+                Coordenada xy(rand() % 100, rand() % 100);
+                a.addVertice(xy);
+            }
+            poligonos[x]=a;
+        }else{
+            for (int i = 0; i < rand() % 100; ++i) {
+                Coordenada xy(rand() % 100, rand() % 100);
+                a.addVertice(xy);
+            }
+            poligonos[x]=a;
+        }
+
     }
-    a.printVertice();
 
-    for (int i = 0; i < 10; ++i) {
-        Coordenada xy(rand() % 100, rand() % 100);
-        b.addVertice(xy);
+    cout <<"Poligonos creados"<<poligonos.size()<<endl;
+    cout << "Vertices creados por los Poligonos: " << a.getVecesMuestF() << endl;
+*/
+
+    int n =100;
+
+    srand(time(NULL));
+    PoligonoIrreg a;
+
+    vector<PoligonoIrreg> poligonos;
+
+    for (int x = 0; x<n ; x++){
+
+            for (int i = 0; i < rand() % 100; ++i) {
+                Coordenada xy(rand() % 100, rand() % 100);
+                a.addVertice(xy);
+            }
+            poligonos.push_back(a);
+
     }
-    b.printVertice();
+
+    cout <<"Poligonos creados"<<poligonos.size()<<endl;
+    cout << "Vertices creados por los Poligonos: " << a.getVecesMuestF() << endl;
 
 
-    cout << "PoligonosCreados: " << a.getVecesMuestF() << endl;
 }
