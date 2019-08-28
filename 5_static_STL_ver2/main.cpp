a39a4ab9dde2d0ba8d4f07174f6bc254cb4d69bb
Luis Fernando Santuario Parra <Santuarioparral@hotmail.com>
2019-08-28 17:46:28 -0500

    Corregido el bug y ya funciona como deberia

--- 5_static_STL_ver2/main.cpp
+++ 5_static_STL_ver2/main.cpp
@@ -6,54 +6,47 @@
 using namespace std;
 
 int main() {
-    /*int n =5000;
+    /*int n = 100000;
 
-    srand(time(NULL));
-    PoligonoIrreg a;
+    PoligonoIrreg b;
 
     vector<PoligonoIrreg> poligonos;
-    poligonos.reserve(10);
+    poligonos.reserve(n);
 
-    for (int x = 0; x<n ; x++){
-        if(poligonos.size()<n){
-            poligonos.resize(poligonos.size()+1);
-            for (int i = 0; i < rand() % 100; ++i) {
-                Coordenada xy(rand() % 100, rand() % 100);
-                a.addVertice(xy);
-            }
-            poligonos[x]=a;
-        }else{
-            for (int i = 0; i < rand() % 100; ++i) {
-                Coordenada xy(rand() % 100, rand() % 100);
-                a.addVertice(xy);
-            }
-            poligonos[x]=a;
+    for (int x = 0; x < n; x++) {
+        int r = rand() % 100;
+        PoligonoIrreg a;
+        for (int i = 0; i < r; ++i) {
+
+            Coordenada xy(rand() % 10, rand() % 10);
+            a.addVertice(xy);
         }
+        poligonos[x] = a;
 
     }
 
-    cout <<"Poligonos creados: "<<poligonos.size()<<endl;
-    cout << "Vertices creados por los Poligonos: " << a.getVecesMuestF() << endl;
-*/
-    int n =8000;
+    cout << "Poligonos creados: " << n << endl;
+    cout << "Vertices creados por los Poligonos: " << b.getVecesMuestF() << endl;
 
-    srand(time(NULL));
+    return 0;*/
+
+    int n = 100000;
+
     PoligonoIrreg a;
 
     vector<PoligonoIrreg> poligonos;
 
-    for (int x = 0; x<n ; x++){
-
-            for (int i = 0; i < rand() % 100; ++i) {
-                Coordenada xy(rand() % 100, rand() % 100);
-                a.addVertice(xy);
-            }
-            poligonos.push_back(a);
+    for (int x = 0; x < n; x++) {
+        int r = rand() % 100;
+        PoligonoIrreg b;
+        for (int i = 0; i < r; ++i) {
+            Coordenada xy(rand() % 10, rand() % 10);
+            b.addVertice(xy);
+        }
+        poligonos.push_back(b);
 
     }
 
-    cout << "Poligonos creados "<<poligonos.size()<<endl;
+    cout << "Poligonos creados " << poligonos.size() << endl;
     cout << "Vertices creados por los Poligonos: " << a.getVecesMuestF() << endl;
-
-
 }
