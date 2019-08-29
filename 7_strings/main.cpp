72eba4ba51d36f0ac02c7dedb872b7a7cf77fe63
Luis Fernando Santuario Parra <Santuarioparral@hotmail.com>
2019-08-29 08:55:05 -0500

    Creado proyecto Strings

---
+++ 7_strings/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    string line ;
+    cin >> n;
+    getline(cin, line);
+    cout<<n<<endl;
+    cout<<line<<endl;
+
+}
