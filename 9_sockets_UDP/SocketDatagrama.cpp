342b015155a2b0cda3fa4930030098792571fc5f
Luis Fernando Santuario Parra <Santuarioparral@hotmail.com>
2019-09-25 10:02:13 -0500

    summited

--- 9_sockets_UDP/SocketDatagrama.cpp
+++ 9_sockets_UDP/SocketDatagrama.cpp
@@ -7,21 +7,20 @@
 #include "PaqueteDatagrama.h"
 
 SocketDatagrama::SocketDatagrama(int PUERTO) {
-    puerto = PUERTO;
     s=socket(AF_INET,SOCK_DGRAM,0);
-    direccionForanea.sin_family =AF_INET;
-    direccionForanea.sin_port = htons(p.obtienePuerto());
-    direccionForanea.sin_addr.s_addr = INADDR_ANY;
+    direccionForanea.sin_family = AF_INET;
+    direccionForanea.sin_port = htons(PUERTO);
+    direccionForanea.sin_addr.s_addr = htons(INADDR_ANY);
+    direccionLocal.sin_addr.s_addr = htons(INADDR_ANY);
     bind(s,(struct sockaddr *) &direccionForanea, sizeof(direccionForanea));
 }
 int SocketDatagrama::envia(PaqueteDatagrama &p) {
-
     sendto(s,p.obtieneDatos(),p.obtieneLongitud(),0,(struct sockaddr *)&direccionForanea, sizeof(direccionForanea));
     recvfrom(s, (char *) &res, sizeof(int), 0, NULL, NULL);
 
 }
 int SocketDatagrama::recibe(PaqueteDatagrama &p) {
-
+    recvfrom(s, );
 }
 SocketDatagrama::~SocketDatagrama() {
 }
